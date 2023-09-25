5565bacdfd3ac4d07ef8d018f168898c02e556c7
Dhanushkumar13 <dhanushpappu99@gmail.com>
2023-09-25 21:19:21 +0530

    added time stamp

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { error } = require('node:console');
 const app = express()
 const fs = require("node:fs/promises");
 const { urlToHttpOptions } = require('node:url');
+const path = require('path');
+const folderPath = './files';
 
 app.get('/',(request,response,next)=>{
     console.log('I am get request')
@@ -10,8 +13,30 @@ app.get('/',(request,response,next)=>{
     });
 });
 
+//TO GET ALL THE FILE SYSTEMS PRESENT IN A PARTICULAR FILE USING READDIR
+//ENDPOINT: http://localhost:5000/api/view
+
+app.get('/view',(request,response,next)=>{
+    fs.readdir(folderPath, (error, files)=>{
+        if(error){
+            return response.status(400).json({
+                Message: "Error Occured!",
+                result: error
+            })
+        }else{
+            const textfile = files.filter((file)=> path.extname(file).toLowerCase === '.txt')
+            response.json({textfile})
+        }
+    })
+})
+
+
+
+
+//TO CREATE FILE SYSTEM 
+// ENDPOINT: http://localhost:5000/api/createFile
 app.get("/createFile", async (request,response,next)=>{
-    const fileName = `${new Date().getDate()}-Time(${new Date().getHours()}-${new Date().getMinutes()})`;
+    const fileName = `${new Date().getDate()}  ${new Date().getMonth()}- Time(${new Date().getHours()}-${new Date().getMinutes()})`;
     try {
          await fs.appendFile(`files/${fileName}.txt`,`${new Date()}`); 
          return response.status(200).json({
